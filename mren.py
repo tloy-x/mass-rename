b7d8419dc69de06941e77f387a94cab33610e328
Tanner Loy <theo94x@gmail.com>
2019-12-11 09:14:16 -0500

    Added '-n' argument, changed usage message, made executable.

--- mren.py
+++ mren.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python3
+
 import os
 import glob
 import sys
 import argparse
 
-parser = argparse.ArgumentParser(description='Rename all files of chosen type in a directory.')
-parser.add_argument('filetype', type=str, help='Choose filetype for all the files you want to rename.')
-parser.add_argument('newname', type=str, help='Choose new name for all files chosen.')
+parser = argparse.ArgumentParser(usage='mren.py filetpe newname [-h] [-v] [-n <int>]', description='rename all files of chosen type in a directory.')
+parser.add_argument('filetype', type=str, help='choose filetype for all the files you want to rename.')
+parser.add_argument('newname', type=str, help='choose new name for all files chosen.')
 
-parser.add_argument('-v', '--verbose', action='store_true', help='Make output verbose.')
+parser.add_argument('-v', '--verbose', action='store_true', help='make output verbose.')
+parser.add_argument('-n', '--number', type=int, metavar='<int>', help='set number of placeholder digits in filenumber. default is 4. maximum is 10.')
 args = parser.parse_args()
 
 ftype = args.filetype
@@ -21,12 +24,20 @@ files = []
 
 count = 0
 
+digitVal = args.number
+if args.number is None:
+    digitVal = 4
+elif args.number > 10:
+    parser.error('-n must be 10 or less. Come on.')
+elif args.number < 1:
+    parser.error('-n must be greater than 0.')
+
 for file in glob.glob('*.' + args.filetype):
     files.append(file)
 
 for i in files:
    count += 1
-   filenum = '{:0>4}'.format(count)
+   filenum = str(count).zfill(digitVal)
    os.rename(i, args.newname + str(filenum) + '.' + args.filetype)
    if args.verbose:
        print(f"Renaming {i} to {args.newname + str(filenum)}.{args.filetype}")
