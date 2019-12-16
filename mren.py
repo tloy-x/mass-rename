f09b1b8c635a50673fda34987d4a657bfc101689
tloy-x <theo94x@gmail.com>
2019-12-15 23:34:13 -0500

    Added version information, cleaned up formatting

--- mren.py
+++ mren.py
@@ -5,12 +5,13 @@ import glob
 import sys
 import argparse
 
-parser = argparse.ArgumentParser(usage='mren filetype newname [-h] [-v] [-n <int>]', description='rename all files of chosen type in a directory.')
-parser.add_argument('filetype', type=str, help='choose filetype for all the files you want to rename.')
-parser.add_argument('newname', type=str, help='choose new name for all files chosen.')
-parser.add_argument('-v', '--verbose', action='store_true', help='make output verbose.')
-parser.add_argument('-n', '--number', type=int, metavar='<int>', help='set number of placeholder digits in filenumber. default is 4. maximum is 10.')
-parser.add_argument('-d','--directory', type=str, metavar='<directory>', help='Specify a directory. Default is current working directory.')
+parser = argparse.ArgumentParser(usage='mren filetype newname [-h] [-v] [-n <int>]', description='rename all files of chosen type in a directory')
+parser.add_argument('filetype', type=str, help='choose filetype for all the files you want to rename')
+parser.add_argument('newname', type=str, help='choose new name for all files chosen')
+parser.add_argument('-v', '--verbose', action='store_true', help='make output verbose')
+parser.add_argument('-V', '--version', action='version', version='mren 1.0', help='show program\'s version number and exit')
+parser.add_argument('-n', '--number', type=int, metavar='<int>', help='set number of placeholder digits in filenumber. default is 4, maximum is 10')
+parser.add_argument('-d','--directory', type=str, metavar='<directory>', help='specify a directory. Default is current working directory')
 args = parser.parse_args()
 
 ftype = args.filetype
@@ -25,6 +26,9 @@ if args.directory is not None:
 files = []
 
 count = 0
+
+if args.verbose: 
+    print('\n-mren version 1.0-\n')
 
 digitVal = args.number
 if args.number is None:
@@ -42,5 +46,5 @@ for i in files:
    filenum = str(count).zfill(digitVal)
    os.rename(i, args.newname + str(filenum) + '.' + args.filetype)
    if args.verbose:
-       print(f"Renaming {i} to {args.newname + str(filenum)}.{args.filetype}")
-print('Done')
+       print(f"Renaming {i} to {args.newname + str(filenum)}.{args.filetype}...")
+print('\nDone')
